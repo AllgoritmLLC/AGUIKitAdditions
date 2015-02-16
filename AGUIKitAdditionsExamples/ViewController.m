fc892103ef6d0a900b8cb4e6019a7d7c1b1ea97b
v.bezugly <valnocorner@gmail.com>
2015-02-16 12:22:05 +0300

    test

--- AGUIKitAdditionsExamples/ViewController.m
+++ AGUIKitAdditionsExamples/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "AGUIKitAdditions.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [self showActivity];
+    
+    __weak typeof(self) __self = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [__self hideActivity];
+
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self showActivity];
+
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [__self hideActivity];
+                ViewController* ctrl = [__self.storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+                [__self.navigationController pushViewController:ctrl animated:YES];
+            });
+        });
+    });
 }
 
 - (void)didReceiveMemoryWarning {
