71ffc828faf043acf720212ed04ff4355709dcc7
v.bezugly <valnocorner@gmail.com>
2015-02-16 10:51:27 +0300

    added project

---
+++ AGUIKitAdditionsExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGUIKitAdditionsExamples
+//
+//  Created by develop on 16/02/15.
+//  Copyright (c) 2015 Allgoritm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
