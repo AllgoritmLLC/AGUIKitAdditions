fcd3e82cdf52800eae3524440eb2bc7b8820711c
v.bezugly <valnocorner@gmail.com>
2015-02-16 17:33:57 +0300

    examples

--- AGUIKitAdditionsExamples/ViewController.m
+++ AGUIKitAdditionsExamples/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UITableView* tableView;
+
 @end
 
 @implementation ViewController
@@ -19,25 +21,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView* tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 500)];
+    tableHeaderView.backgroundColor = [UIColor yellowColor];
+    
+    UILabel* lbHello = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 21)];
+    lbHello.text = @"Hello";
+    lbHello.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [tableHeaderView addSubview:lbHello];
+    [tableHeaderView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[label]-0-|"
+                                                                            options:0
+                                                                            metrics:nil
+                                                                              views:@{@"label":    lbHello}]];
+    [tableHeaderView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[label]"
+                                                                            options:0
+                                                                            metrics:nil
+                                                                              views:@{@"label":    lbHello}]];
+    NSLayoutConstraint* constraint = [NSLayoutConstraint constraintWithItem:tableHeaderView
+                                                                  attribute:NSLayoutAttributeBottom
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:lbHello
+                                                                  attribute:NSLayoutAttributeBottom
+                                                                 multiplier:1
+                                                                   constant:0];
+    constraint.priority = 999;
+    [tableHeaderView addConstraint:constraint];
+    constraint = [NSLayoutConstraint constraintWithItem:tableHeaderView
+                                              attribute:NSLayoutAttributeBottom
+                                              relatedBy:NSLayoutRelationGreaterThanOrEqual
+                                                 toItem:lbHello
+                                              attribute:NSLayoutAttributeBottom
+                                             multiplier:1
+                                               constant:0];
+    constraint.priority = 1000;
+    [tableHeaderView addConstraint:constraint];
+    
+    self.tableView.tableHeaderView = tableHeaderView;
 }
 
 -(void)viewDidAppear:(BOOL)animated{
-    [self showActivity];
-    
     __weak typeof(self) __self = self;
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [__self hideActivity];
-
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self showActivity];
-
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [__self hideActivity];
-                ViewController* ctrl = [__self.storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
-                [__self.navigationController pushViewController:ctrl animated:YES];
-            });
-        });
+        [__self.tableView sizeTableHeaderViewToFit];
     });
+    
+//    [self showActivity];
+//    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [__self hideActivity];
+//
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            [self showActivity];
+//
+//            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//                [__self hideActivity];
+//                ViewController* ctrl = [__self.storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+//                [__self.navigationController pushViewController:ctrl animated:YES];
+//            });
+//        });
+//    });
 }
 
 - (void)didReceiveMemoryWarning {
